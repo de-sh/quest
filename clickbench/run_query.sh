332ab883801dbb9f1a5a5af43c00251cdfc2a69a
Devdutt Shenoi <devdutt@parseable.com>
2024-12-17 22:49:36 +0530

    feat: demark expectation

--- clickbench/run_query.sh
+++ clickbench/run_query.sh
@@ -3,15 +3,16 @@
 QUERY_NUM=1
 cat 'queries.sql' | while read -r QUERY; do
 
+    printf "$QUERY_NUM: $QUERY => "
     JSON=$(jq -n --arg query "$QUERY" \
-        '{query: $query, startTime: "2024-08-22T00:00:00.000Z", endTime: "2024-08-22T23:00:00.000Z"}')
+        '{query: $query, startTime: "2024-10-29T00:00:00.000Z", endTime: "2024-11-29T23:00:00.000Z"}')
+
     start_time=$(date +%s%3N)
-
-    ES_RSP= curl  -H "Content-Type: application/json" -k -XPOST -u "admin:admin" "http://3.140.239.140:8000/api/v1/query" --data "${JSON}"
-
+    curl  -H "Content-Type: application/json" -k -XPOST -u "admin:admin" "$P_URL/api/v1/query" --data "${JSON}"
     end_time=$(date +%s%3N)
     elapsed_time=$((end_time - start_time))
 
+    echo " (${elapsed_time}ms)"
     echo "${QUERY_NUM},${elapsed_time}" >> result.csv
 
     QUERY_NUM=$((QUERY_NUM + 1))
