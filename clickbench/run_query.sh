3cc6214462875b2ae73b94eef0404faa4514905e
Devdutt Shenoi <devdutt@parseable.com>
2024-12-18 11:35:44 +0530

    refactor: query extraction from json

--- clickbench/run_query.sh
+++ clickbench/run_query.sh
@@ -5,21 +5,21 @@ JSON_FILE="expected.json"
 QUERY_NUM=1
 
 # Parse the JSON file and iterate over each query-response pair
-jq -c '.[]' "$JSON_FILE" | while read -r item; do
+jq -c '.queries[]' "$JSON_FILE" | while read -r query; do
     # Extract the query and expectation from the current JSON object
-    QUERY=$(echo "$item" | jq -r '.query')
-    RESPONSE=$(echo "$item" | jq -r '.response')
+    QUERY=$(echo "$query" | jq -r '.query')
+    RESPONSE=$(echo "$query" | jq -r '.response')
 
     printf "$QUERY_NUM: $QUERY => "
 
     # Construct the JSON payload for the request
     JSON=$(jq -n --arg query "$QUERY" \
-        '{query: $query, startTime: "2024-10-29T00:00:00.000Z", endTime: "2024-11-29T23:00:00.000Z"}')
+        '{query: $query, startTime: "$START_TIME", endTime: "$END_TIME"}')
 
     # Measure the time taken to execute the query
-    start_time=$(date +%s%3)
-    curl  -H "Content-Type: application/json" -k -XPOST -u "admin:admin" "$P_URL/api/v1/query" --data "${JSON}"
-    end_time=$(date +%s%3)
+    start_time=$(date +%s%3N)
+    curl  -H "Content-Type: application/json" -k -XPOST -u "$P_USERNAME:$P_PASSWORD" "$P_URL/api/v1/query" --data "${JSON}"
+    end_time=$(date +%s%3N)
     elapsed_time=$((end_time - start_time))
 
     echo " (${elapsed_time}ms)"
